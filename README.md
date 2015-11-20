15d8b4392284a4af809a0747c7cef0612dbe2ec5
Chaz Straney <chaz@plaidpotion.com>
2015-11-20 11:54:04 -0500

    procs for contextualization, fix enormous stupid bug

--- README.md
+++ README.md
@@ -24,16 +24,62 @@ i.thingy # => "widget"
 i.doodad # => ["lol", "rofl"]
 ```
 
+You can contextualize domain objects as follows:
+
+```
+class Base
+  include Babby::MetaDSL
+
+  dsl_method :contextualize, type: :proc
+
+  attr_accessor :entity
+
+  def initialize(instance)
+    @entity = instance
+  end
+
+  def generate
+    puts "path: #{contextualize[:path]} content: #{contextualize[:content]}"
+  end
+end
+
+class PooGenerator < Base
+  contextualize Proc.new { |instance| { path: instance.path, content: instance.content } }
+end
+
+class Thing
+  attr_accessor :path, :content
+
+  def initialize(path, content)
+    @path = path
+    @content = content
+  end
+end
+
+e = Thing.new('lol what', "poopsticks")
+i = PooGenerator.new(e)
+
+puts i.generate
+```
+
 ### But inheritance is yucky
-Yes, but not for is-a relationships.
+Yes, but not for is-a relationships. Besides, dogmatism is yucky too. :3
+
 
 ## Development
 
 ### TODOs
-* add specs
+* add specs!!!
+* Allow parameterization of derived class entity - see babby.rb:21
 * add inflection/pluralization for arrays, with manual getter specification as a backup.
+* Add ability to validate type of argument to dsl method
+* Add ability to validate presence of hash keys on hash arguments
 
 ## License
 
 The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
 
+## Changelog
+
+0.2.0 - Fixed glaring bug in 0.1.0 (static map missing from module)
+      - Add proc
--- lib/babby.rb
+++ lib/babby.rb
@@ -1,4 +1,8 @@
-require "babby/version"
+#require "babby/version"
+
+STATE_TYPE_MAP = {
+  array: Array
+}
 
 module Babby
   module MetaDSL
@@ -14,6 +18,10 @@ module Babby
 
     module ClassMethods
 
+      def apply_to entity_method_name
+        # Set the name of the method to retrieve the entity bound to the instance, as a symbol
+      end
+
       def dsl_method name, type: nil
         state_type = type ? STATE_TYPE_MAP[type] : nil
 
@@ -28,20 +36,29 @@ module Babby
         else
           define_singleton_method name do |value| # define 'setter' for derived class - i.e. DSL 'setup' method a la attr_accessor
             if not self.instance_variable_get  "@#{name}"
-              self.instance_variable_set "@#{name}", type ? state_type.new : nil
+              self.instance_variable_set "@#{name}", state_type ? state_type.new : nil
             end
 
             self.instance_variable_set "@#{name}", value  # Set the underlying class instance variable to the desired value
           end
         end
+
         define_singleton_method "get_#{name}".to_sym do
           self.instance_variable_get "@#{name}"
         end
 
-        define_method name do
-          self.class.send "get_#{name}".to_sym
+        if type == :proc
+          define_method name do
+            proc = self.class.send "get_#{name}".to_sym
+            proc.call(entity)
+          end
+        else
+          define_method name do
+            self.class.send "get_#{name}".to_sym
+          end
         end
       end
     end
   end
-end
+end              
+
